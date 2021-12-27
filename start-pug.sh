80242a1e5d5bcf1c17750828a159450156e4251d
Denis Dechev <denisde4ev@vivaldi.net>
2021-12-27 18:36:40 +0200

    add index.html with list of files

---
+++ start-pug.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -x
+set -u
+
+cd "${0%/*}" || exit
+
+o=${0##*/}
+o=${o%".sh"}
+
+
+
+pug -w . -O "$(
+
+
+	printf %s\\n "{${o+
+
+		HTML_FILES: "$(
+			find ./[!._%]*[!~] -iname '*.html' -print0 | node -p '
+				JSON.stringify(
+					fs.readFileSync(0).toString()
+						.match(/[^\0]+/g)
+						.map(v=>v.replace(/^.\//,""))
+				)
+			'
+		)"
+
+		}}"
+
+
+)"
+
