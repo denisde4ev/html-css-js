25cff3d9e452b10cca2e6fa13493b97ee8bc9d07
Denis Dechev <denisde4ev@vivaldi.net>
2022-05-06 02:59:13 +0300

    2022-05-06

--- js/lib/min.js
+++ js/lib/min.js
@@ -5,3 +5,4 @@ $.new = document.createElement.bind(document)
 Node.prototype.val = function (key, value){ this[key] = value; return this; }
 Node.prototype.attr = function (){ this.setAttribute.apply(this,arguments); return this; }
 Node.prototype.on = function (event, fn){ this.addEventListener(event, fn, false); return this; }
+NodeList.prototype.on = function (event, fn){ this.forEach( function(v){v.on(event,fn)} ); return this; }
