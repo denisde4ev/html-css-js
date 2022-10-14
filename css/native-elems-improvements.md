731494c8e9f955b9a0075858d83203081b2debc9
Denis Dechev <denisde4ev@vivaldi.net>
2022-10-14 21:41:48 +0300

    ➔A css/native-elems-improvements.md;A css/stylus/abv-homepage-only-login.css;A css/stylus/abv.css;A js/user-scripts/abv-login-autofucus.js;

---
+++ css/native-elems-improvements.md
@@ -0,0 +1,3 @@
+
+## a good example of `input[type=checkbox]` to visually as animated switch
+https://github.com/iamshaunjp/Coding-Challenges/blob/challenge-3-solution/styles.css
---
+++ js/user-scripts/abv-login-autofucus.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         abv.bg select login->username
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       denisde4ev
+// @match        https://www.abv.bg/
+// @icon         https://icons.duckduckgo.com/ip2/abv.bg.ico
+// @grant        none
+// @run-at document-end
+// ==/UserScript==
+
+username.focus();
