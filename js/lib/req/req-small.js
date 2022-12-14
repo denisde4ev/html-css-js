128ea54049f5f53e7cbbc9b6a3643eee04529d23
denisde4ev <git@denisde4ev.xyz>
2022-12-14 21:50:09 +0200

    c

--- js/lib/req/req-small.js
+++ js/lib/req/req-small.js
@@ -3,14 +3,14 @@
 function req(name) {
 	if (req.loaded[name]) return req.loaded[name];
 
-	var module = {exports: {},req: req};
+	var module = {exports: {}};
 
 	Function(
 		'module',
-		Hget( req.basepath+name.replace(/\.js$|$/,'.js') )
+		Hget( req.basepath + '/' + name.replace(/\.js$|$/,'.js') )
 	)(module);
 
 	return req.loaded[name] = module.exports;
 }
 req.loaded = {};
-req.basepath = '/assets/js/'
+req.basepath = '/assets/js'
