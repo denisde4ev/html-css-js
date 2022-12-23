600e06d64bd94fb0bf45d3270046f65f9a62b463
denisde4ev (pc57) <git-from-pc57@denisde4ev.xyz>
2022-12-23 13:48:42 +0200

    req.loaded can be used for circular dependency

--- js/lib/req/req.js
+++ js/lib/req/req.js
@@ -8,30 +8,56 @@ if (!Object.hasOwn) {
 
 
 var req = function(name) {
-	//if (name.endsWith('.js')) name = name.slice(0,-3);
-	if (Object.hasOwn(req.loaded,name)) {
-		return req.loaded[name];
+	if (name.endsWith('.js')) name = name.slice(0,-3);
+	if (Object.hasOwn(req.loaded, name)) {
+		let loading = req.loading[name];
+		if (!loading) {
+			return req.loaded[name];
+		} else if (loading === true) {
+			console.warn('req used on sync loading lib: ' + name + '!', new Error('')); // TODO: test + compare to nodejs require
+			return req.loaded[name];
+		} else {
+			// probably async loading, now we need to force load it
+			console.warn('req used on loading lib: ' + name, new Error(''));
+			try {
+				loading.abort();
+			} catch (e) {}
+		}
+	} else {
+		console.warn('req used on missing lib');
 	}
 
-	console.warn('req is acsessing missing lib');
 
-	var x = new XMLHttpRequest();
-	x.open('GET', req.basepath + '/' + name + '.js', false);
-	x.send();
-	if (x.status !== 200) throw new Error('module req HTTP status: '+x.status, { x:x, name:name });
-	var code = x.responseText;
+	try {
+		req.loading[name] = true;
+		var module = { exports: req.loaded[name] || {} };
+		req.loaded[name] = module.exports;
 
-	return req.loaded[name] = req.evalmodule(code);
+		var x = new XMLHttpRequest();
+		x.open('GET', req.basepath + '/' + name + '.js', false);
+		x.send();
+		if (x.status !== 200) throw new Error('module req HTTP status: ' + x.status, { x:x, name:name });
+		let code = x.responseText;
+
+
+		var exported = req.evalmodule(code, module);
+		req.loading[name] = false;
+		return req.loaded[name] = exported;
+	} catch (e) {
+		req.loading[name] = false;
+		console.error('failed to sync load lib: ' + name);
+		throw e;
+	}
 };
+req.loading = {}; // key is lib name, val is boolean=true|promise; true = sync load, promise = async load
 
-
-req.evalmodule = code => {
-	var _givenexports = {}, module = {exports: _givenexports};
-	var returnedValue = Function('module, code', 'return eval(code);')(module, code);
+req.evalmodule = (code, module) => {
+	var exports = module.exports;
+	var returnedValue = Function('code, module, exports', 'return eval(code);')(code, module, exports);
 
 	return (
-		 _givenexports !== module.exports ||
-		 0 < Object.keys(module.exports).length
+		 exports !== module.exports // when module.exports is overwritten, note: this will breake for circular dependency
+		 || 0 < Object.keys(module.exports).length
 		 	? module.exports
 		 	: returnedValue
 	);
@@ -41,25 +67,62 @@ req.loaded = {};
 req.basepath = '/' //{__proto__: null}; // must be defined
 
 
+/*
 try {
-	req.get = new Function( 1 +    atob('YXN5bmMgbmFtZSA9PiB7CglpZiAobmFtZS5lbmRzV2l0aCgnLmpzJykpIG5hbWUgPSBuYW1lLnNsaWNlKDAsLTMpOyAKCglpZiAocmVxLmxvYWRlZFtuYW1lXSkgewoJCWNvbnNvbGUud2FybihuZXcgRXJyb3IoJ2xpYiByZXF1aXJlZCBtdWx0aXBsZSB0aW1lOiAnK25hbWUpKTsKCQlyZXR1cm4gcmVxLmxvYWRlZFtuYW1lXTsKCX0KCgl2YXIgY29kZSA9IGF3YWl0IChhd2FpdCBmZXRjaChyZXEuYmFzZXBhdGggKyAnLycgKyBuYW1lICsgJy5qcycpKS50ZXh0KCk7CglyZXR1cm4gcmVxLmxvYWRlZFtuYW1lXSA9IHJlcS5ldmFsbW9kdWxlKGNvZGUpOwp9Ow=='));
+	req.get = new Function(atob(...));
 } catch (e) {
+	// todo: better error handling
 	let _Promise = window.Promise || function(cb) { var r; cb(a => r = a); this.then = function(cb) { this.then=0; cb(r); }; };
 	req.get = name => new _Promise(r => r(req(name)));
 }
-/*
+/*/
+if (!globalThis.AbortController) throw 'missing AbortController';
 req.get = async name => {
 	if (name.endsWith('.js')) name = name.slice(0,-3); 
 
-	if (req.loaded[name]) {
-		console.warn(new Error('lib required multiple time: '+name));
-		return req.loaded[name];
+	if (Object.hasOwn(req.loaded, name)) {
+		let loading = req.loading[name];
+		if (!loading) {
+			return req.loaded[name];
+		} else if (loading === true) {
+			console.warn('req.get used on sync loading lib: ' + name + ', this should not be possible!', new Error(''));
+			return req.loaded[name];
+		} else {
+			// probably async loading, now we need to force load it
+			console.warn('req.get used on loading lib: ' + name, new Error(''));
+			return req.loaded[name];
+		}
 	}
 
-	var code = await (await fetch(req.basepath + '/' + name + '.js')).text();
-	return req.loaded[name] = req.evalmodule(code);
+
+	try {
+		var loading = {};
+		if (Object.hasOwn(req.loading, name)) console.error(req.loading[name], new Error(4)); // DEBUG
+		req.loading[name] = loading; // note: req.loading[name] will always be *missing prop*
+		if (Object.hasOwn(req.loaded , name)) console.error(req.loaded [name], new Error(4)); // DEBUG
+		var module = { exports: {} }; // note: req.loaded[name] here will always be *missing prop*
+		req.loaded[name] = module.exports;
+
+		var controller = new AbortController();
+		loading.controller = controller;
+		var signal = controller.signal;
+		loading.signal = signal;
+
+		var fetching = fetch(req.basepath + '/' + name + '.js', { signal });
+
+		var code = await (await fetching).text();
+
+		var exported = req.evalmodule(code);
+		req.loading[name] = false;
+
+		return req.loaded[name] = exported;
+	} catch (e) {
+		req.loading[name] = false;
+		console.error('failed to async load lib: ' + name);
+		throw e;
+	}
 };
-*/
+//*/
 
 
 
