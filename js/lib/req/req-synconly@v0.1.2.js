50855c542d58c60fe416a901a6e8b792bf9d3db9
denisde4ev <git@denisde4ev.xyz>
2022-12-29 00:13:55 +0200

    c

--- js/lib/req/req-synconly@v0.1.2.js
+++ js/lib/req/req-synconly@v0.1.2.js
@@ -3,8 +3,8 @@
 
 _m = {};
 var req = n => {
-	if ( e=_m[{n,u}=req.p(n),n]/* && Object.keys(_m[n]).length*/ ) return e;
-	var u, e=_m[n]=e||{}, m={exports:e}, x=new XMLHttpRequest(); x.open('GET',u,false); x.send(); // don't set request Content-Type. or it'll discard prefethed cache
+	if ( e=_m[n]/* && Object.keys(_m[n]).length*/ ) return e;
+	var u=req.p(n), e=_m[n]=e||{}, m={exports:e}, x=new XMLHttpRequest(); x.open('GET',u,false); x.send(); // don't set request Content-Type. or it'll discard prefethed cache
 	if ( x.status!==200 || !/^(application|text)\/(x-)?javascript($|;)/i.test(x.getResponseHeader('Content-Type')) ) {
 		throw new Error('bad Content-Type');
 	}
@@ -13,4 +13,5 @@ var req = n => {
 	return e;
 };
 req.b = {__proto__:null}; // req.basepath // value example: 'https://cdn.jsdelivr.net/gh/USER/REPO/LIBDIR', for mine libs: 'https://cdn.jsdelivr.net/gh/denisde4ev/html-css-js/js/lib'
-req.p = (n,u) => ({n:n.replace(/^(https:\/\/)?(.*?)(\.js)?$/,(O,p,n,j)=>(u=p?O:req.b+'/'+n+(j||'.js'),n)),u}); // req.parse
+req.p = n => n.replace(/^(https:\/\/)?(.*?)(\.js)?$/,(O,p,n,j)=>p?O:req.b+'/'+n+(j||'.js')); // req.parse
+// export req.parse 
