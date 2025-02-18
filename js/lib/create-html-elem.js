156617da2b251bde8ab60cc9a4d5c0d2159635b3
denisde4ev <git@denisde4ev.xyz>
2025-02-19 01:54:14 +0200

    c

--- js/lib/create-html-elem.js
+++ js/lib/create-html-elem.js
@@ -1,9 +1,10 @@
 
-function htmlElement(text){
-	htmlElement.div.innerHTML = text;
-	return htmlElement.div.childElementCount === 1
+function htmlElement(text) {
+	this.div.innerHTML = text;
+	return (
+		htmlElement.div.childElementCount === 1
 		?htmlElement.div.firstChild
 		:htmlElement.div.children
-	;
+	);
 }
 htmlElement.div = document.createElement('div');
--- js/lib/document_createElementHTML/1.js
+++ js/lib/document_createElementHTML/1.js
@@ -1,6 +1,7 @@
+// better use: lib/create-html-elem.js
 
 function document_createElementHTML(html) {
 	document_createElementHTML__tmp.innerHTML = html;
 	return document_createElementHTML__tmp.firstElementChild;
 }
-document_createElementHTML__tmp = document.createElement('div');
+var document_createElementHTML__tmp = document.createElement('div');
